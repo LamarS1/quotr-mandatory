79d31701d17fda9f61dee31cd26e4de98a7da790
Dan Okkels Brendstrup <brendstrup@gmail.com>
2021-10-05 21:41:14 +0200

    feat: add recipe model and routes

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,14 @@ import { useEffect, useState } from "react";
 const API_URL = process.env.REACT_APP_API;
 
 function App() {
-  const [data, setData] = useState("No data :(");
+  const [data, setData] = useState([]);
 
   useEffect(() => {
     async function getData() {
-      const url = `${API_URL}/hello`;
+      const url = `${API_URL}/recipes`;
       const response = await fetch(url);
       const data = await response.json();
-      setData(data.msg);
+      setData(data);
     }
     getData();
   }, []);
@@ -17,7 +17,16 @@ function App() {
   return (
     <>
       <h1>MERN App!</h1>
-      <p>Data from server: {data}</p>
+      <p>Data from server:</p>
+      <ul>
+        {data.map((item) => {
+          return (
+            <li key={item._id}>
+              <strong>{item.title}</strong> (<code>{item._id}</code>)
+            </li>
+          );
+        })}
+      </ul>
     </>
   );
 }
